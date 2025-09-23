b0854e6ce87012d4ecfe72b1e730e841de636097
Daniel Foscarini <76021871+tocsindata@users.noreply.github.com>
2025-09-23 11:53:08 -0700

    sync

--- rebrand/configure.php
+++ rebrand/configure.php
@@ -1,5 +1,13 @@
 <?php
 // usersc/plugins/rebrand/configure.php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+function rebrand_redirect_back($usUrlRoot) {
+  header('Location: ' . rtrim($usUrlRoot, '/') . '/usersc/plugins/rebrand/configure.php');
+  exit;
+}
 
 // Robustly locate /users/init.php from anywhere under the plugin folder
 $init = null;
