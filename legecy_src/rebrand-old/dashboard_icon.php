f197e6f70e4dcedddb69900fc2e5ea55754ee2d7
Daniel Foscarini <76021871+tocsindata@users.noreply.github.com>
2025-10-02 12:50:33 -0700

    cleaned up old code

--- legecy_src/rebrand-old/dashboard_icon.php
+++
@@ -1,8 +0,0 @@
-<?php
-// file : public_html/usersc/plugins/rebrand/dashboard_icon.php
-?>
-<a href="<?=$us_url_root?>users/admin.php"><div class="col-md-1 col-sm-3 col-sm-6 col-centered">
-  <div class="panel panel-default">
-    <i class="fa fa-qq fa-2x"></i><br>Rebrand</li>
-  </div>
-</div></a> 
--- legecy_src/rebrand2/footer.php
+++
@@ -1,3 +0,0 @@
-<?php
-// file : public_html/usersc/plugins/rebrand/footer.php
-// null nothing to do here
